601b91363ccde13186e40b3311d3b923242e43fc
Kay <Kay>
2022-05-19 13:27:00 +0200

    Preview update recommeded by studio

--- Project1/Data/ApplicationDbContext.cs
+++ Project1/Data/ApplicationDbContext.cs
@@ -11,7 +11,6 @@ namespace Project1.Data
         public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
             : base(options, operationalStoreOptions)
         {
-
         }
 
         public DbSet<Computer> Computers { get; set; }
@@ -24,95 +23,102 @@ namespace Project1.Data
         public DbSet<Mousemat> Mousemats { get; set; }
         public DbSet<Screen> Screens { get; set; }
 
-
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<Computer>().HasData(
-                new Computer {  Id = 1,
-                                Title = "Lenovo Workstation",
-                                SubTitle = "Pretty sweet TBH",
-                                Rating = 4,
-                                Image = "https://www.lenovo.com/medias/lenovo-desktop-v530-amd-tower-hero.png?context=bWFzdGVyfHJvb3R8MjA0MjA3fGltYWdlL3BuZ3xoYTQvaGQxLzk5MTA5NjMwNzcxNTAucG5nfDQ0ZGRjZmY0MTQyM2RlNGFjMGFjMzU0MDgxMmYyN2U3OGMwZWM2NDVkMTQ1ZjY1ZmQ1N2Y2OGFmYzE0MGZjOWE",
-                                CPU = "Intel I7",
-                                Manufacturer = "Lenovo",
-                                Ram = "16GB DDR4"   },
-
-                
-
-
-
-
-
-
+                new Computer
+                {
+                    Id = 1,
+                    Title = "Lenovo Workstation",
+                    SubTitle = "Pretty sweet TBH",
+                    Rating = 4,
+                    Image = "https://www.lenovo.com/medias/lenovo-desktop-v530-amd-tower-hero.png?context=bWFzdGVyfHJvb3R8MjA0MjA3fGltYWdlL3BuZ3xoYTQvaGQxLzk5MTA5NjMwNzcxNTAucG5nfDQ0ZGRjZmY0MTQyM2RlNGFjMGFjMzU0MDgxMmYyN2U3OGMwZWM2NDVkMTQ1ZjY1ZmQ1N2Y2OGFmYzE0MGZjOWE",
+                    CPU = "Intel I7",
+                    Manufacturer = "Lenovo",
+                    Ram = "16GB DDR4"
+                }
 
                 );
             builder.Entity<Headset>().HasData(
-                new Headset {   Id = 1,
-                                Title = "Bose 700 Noise Cancelling Headphones Headset",
-                                SubTitle = "Pretty sweet TBH",
-                                Rating = 5,
-                                Image = "",
-                            }
+                new Headset
+                {
+                    Id = 1,
+                    Title = "Bose 700 Noise Cancelling Headphones Headset",
+                    SubTitle = "Pretty sweet TBH",
+                    Rating = 5,
+                    Image = "",
+                }
                 );
             builder.Entity<Keyboard>().HasData(
-                new Keyboard {  Id = 1,
-                                Title = "Corsair K60 Pro",
-                                SubTitle = "Pretty sweet TBH",
-                                Rating = 5,
-                                Image = "",
-                             }
+                new Keyboard
+                {
+                    Id = 1,
+                    Title = "Corsair K60 Pro",
+                    SubTitle = "Pretty sweet TBH",
+                    Rating = 5,
+                    Image = "",
+                }
                 );
             builder.Entity<Keycap>().HasData(
-                new Keycap {    Id = 1,
-                                Title = "Corsair Gaming PBT - Double-shot Keycaps",
-                                SubTitle = "Pretty sweet TBH",
-                                Rating = 3,
-                                Image = "",
-                           }
+                new Keycap
+                {
+                    Id = 1,
+                    Title = "Corsair Gaming PBT - Double-shot Keycaps",
+                    SubTitle = "Pretty sweet TBH",
+                    Rating = 3,
+                    Image = "",
+                }
                 );
             builder.Entity<KeySwitch>().HasData(
-                new KeySwitch {    Id = 1,
-                                   Title = "Corsair keyswitches",
-                                   SubTitle = "Pretty sweet TBH",
-                                   Rating = 4,
-                                   Image = "",
-                              }
+                new KeySwitch
+                {
+                    Id = 1,
+                    Title = "Corsair keyswitches",
+                    SubTitle = "Pretty sweet TBH",
+                    Rating = 4,
+                    Image = "",
+                }
                 );
             builder.Entity<Mice>().HasData(
-                new Mice {  Id = 1,
-                            Title = "Logitech G502 HERO",
-                            SubTitle = "Pretty sweet TBH",
-                            Rating = 4,
-                            Image = "",
-                         }
+                new Mice
+                {
+                    Id = 1,
+                    Title = "Logitech G502 HERO",
+                    SubTitle = "Pretty sweet TBH",
+                    Rating = 4,
+                    Image = "",
+                }
                 );
             builder.Entity<Microphone>().HasData(
-                new Microphone {    Id = 1,
-                                    Title = "Blue Microphones Yeti ",
-                                    SubTitle = "Pretty sweet TBH",
-                                    Rating = 3,
-                                    Image = "",
-                               }
+                new Microphone
+                {
+                    Id = 1,
+                    Title = "Blue Microphones Yeti ",
+                    SubTitle = "Pretty sweet TBH",
+                    Rating = 3,
+                    Image = "",
+                }
                 );
             builder.Entity<Mousemat>().HasData(
-                new Mousemat {  Id = 1,
-                                Title = "Corsair MM300 Pro Premium Spill-Proof Cloth Gaming Extended Muismat",
-                                SubTitle = "Pretty sweet TBH",
-                                Rating = 3,
-                                Image = "",
-                             }
+                new Mousemat
+                {
+                    Id = 1,
+                    Title = "Corsair MM300 Pro Premium Spill-Proof Cloth Gaming Extended Muismat",
+                    SubTitle = "Pretty sweet TBH",
+                    Rating = 3,
+                    Image = "",
+                }
                 );
             builder.Entity<Screen>().HasData(
-                new Screen {    Id = 1,
-                                Title = "MSI Optix G24C4",
-                                SubTitle = "Pretty sweet TBH",
-                                Rating = 4,
-                                Image = "",
-                           }
+                new Screen
+                {
+                    Id = 1,
+                    Title = "MSI Optix G24C4",
+                    SubTitle = "Pretty sweet TBH",
+                    Rating = 4,
+                    Image = "",
+                }
                 );
-
-
-
 
             base.OnModelCreating(builder);
         }
