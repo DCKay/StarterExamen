4a87179f3d3af478390733a7ea37533e6077b78e
Kay <Kay>
2022-05-27 19:21:49 +0200

    changes?

--- Project1/Data/DummyComputer.cs
+++ Project1/Data/DummyComputer.cs
@@ -4,10 +4,31 @@ namespace Project1.Data
 {
     public class DummyComputer : IDummyData
     {
+        //public DummyComputer()
+        //{
+                
+        //}
+
+        //public int Id { get; private set; }
+        //public string Title { get; private set; }
+        //public string SubTitle { get; private set; }
+        //public int Rating { get; private set; }
+        //public string Image { get; private set; }
+        //public string Manufacturer { get; private set; }
+        //public string CPU { get; private set; }
+        //public string Ram { get; private set; }
+        //public string Storage { get; private set; }
+        //public string Name { get; private set; }
+        //public Colour Colour { get; private set; }
+        //public string OS { get; private set; }
+        //public string GPU { get; private set; }
+        //public string PSU { get; private set; }
+        //public bool WiFi { get; private set; }
+
         public void DummyData()
         {
-             new Computer
-                { Id = 2,
+            new Computer
+                { Id = 1,
                 Title = "Lenovo Workstation",
                 SubTitle = "Pretty sweet TBH",
                 Rating = 4,
